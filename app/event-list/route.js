3ca1a7c13d2315fa45699912f1dd6d5a73e470e8
Stanislav Termosa <stanislav.termosa@gmail.com>
2015-06-13 17:05:13 +0300

    Add button for randomly setting images

--- app/event-list/route.js
+++ app/event-list/route.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.store.find('event');
+  },
+  actions: {
+    updateAll (list) {
+      var imgs = ['http://www.redcarpetmonday.com/images/emberlogo1.gif',
+       'http://core0.staticworld.net/images/article/2013/07/ember-logo-100047146-large.jpg',
+       'http://voidcanvas.com/wp-content/uploads/2013/11/ember_logo1.png',
+       'http://www.kino-teatr.ru/art/895/5129.jpg'];
+
+      list.forEach(function (event) {
+        event.set('logo', imgs[Math.floor(Math.random() * 4)]);
+        event.save();
+      });
+    }
   }
 });
