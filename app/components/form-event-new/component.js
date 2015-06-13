f353409218195a5fa9de6171d7fc93d7a9e9011b
Stanislav Termosa <stanislav.termosa@gmail.com>
2015-06-13 12:43:50 +0300

    Setup form to use Firebase

--- app/components/form-event-new/component.js
+++ app/components/form-event-new/component.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   logoUrl: 'http://www.redcarpetmonday.com/images/emberlogo1.gif',
   actions: {
-    newEvent (name, logoUrl, data) {
-      console.info( arguments );
+    newEvent (name, logo, date) {
+      this.sendAction('onEventSubmitted', { name, logo, date });
     }
   }
 });
--- app/models/event.js
+++ app/models/event.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  date: DS.attr('date'),
+  date: DS.attr('string'),
   logo: DS.attr('string')
 });
