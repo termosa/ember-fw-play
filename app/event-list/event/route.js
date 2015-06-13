31d6e21c46f8e8a177ef7f3c0f78fb24bdafb0b9
Stanislav Termosa <stanislav.termosa@gmail.com>
2015-06-13 16:51:30 +0300

    Add ability to remove events

--- app/event-list/event/route.js
+++ app/event-list/event/route.js
@@ -3,5 +3,14 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model (params) {
     return this.store.find('event', params.event_id);
-  }
+  },
+  actions: {
+    onDelete (event) {
+      var route = this;
+      route.store.find('event', event.id).then(function (event) {
+        event.destroyRecord();
+        route.transitionTo('/events');
+      });
+    }
+ }
 });
